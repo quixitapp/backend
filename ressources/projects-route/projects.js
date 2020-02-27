1112171edb8db407fc6c606b9fbb7f4309d4ce43
tjforeman <tylerforeman92@gmail.com>
2020-02-27 13:36:32 -0500

    created and migrated invoice table

--- ressources/projects-route/projects.js
+++ ressources/projects-route/projects.js
@@ -69,7 +69,7 @@ router.post("/", restricted, async  (req, res) => {
 router.delete("/:id", restricted, async  (req, res) => {
   const id = req.params.id
   try {
-    const project = await db.updateProject(id)
+    const project = await db.deleteProject(id)
     res.status(200).json(project)
   } catch ({ message }) {
     console.log(message)
